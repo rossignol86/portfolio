a68b99bd3d2bf689c16baf666af7914f93263676
Franck Rossignolgit config --global user.email ff.rossignol@yahoo.frgit config --global user.name Franck <ff.rossignol@yahoo.fr>
2024-06-19 14:56:38 +0200

    validation

--- admin/listecontact.php
+++ admin/listecontact.php
@@ -38,22 +38,21 @@ mysqli_close($connexion);
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="styles/reset.css">
-    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="stylesheet" href="../styles/styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Righteous&display=swap" rel="stylesheet">  
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Liste des contacts</title>
 </head>
 <body>
-    <div class="container">
+    <div class="bloclistecontact">
         <?php if (!empty($contacts)): ?>
             <?php foreach ($contacts as $contact): ?>
                 <div>
-                    <p class="postes"><?= ($contact['nom']); ?></p>
-                    <p class="postes"><?php echo ($contact['societe']); ?></p>
-                    <p class="mission"><?php echo ($contact['telephone']); ?></p>
-                    <p class="mission"><?php echo ($contact['email']); ?></p>
-                    <p class="mission"><?php echo ($contact['message']); ?></p>
+                    <p class="nomducontact"><?= ($contact['nom']); ?></p>
+                    <p class="societeducontact"><?php echo ($contact['societe']); ?></p>
+                    <p class="telducontact"><?php echo ($contact['telephone']); ?></p>
+                    <p class="telducontact"><?php echo ($contact['email']); ?></p>
+                    <p class="messageducontact"><?php echo ($contact['message']); ?></p>
                     <hr>
                 </div>
             <?php endforeach; ?>
